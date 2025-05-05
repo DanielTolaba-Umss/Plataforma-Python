f5d2ec8460ad7aa84b0103bf9764955830e3c5f4
JhonatanEQ <encinas.jhonatan.dev@gmail.com>
2025-05-04 22:46:24 -0400

    FEAT(Jhonatan): Crear peticiones http post, get y get por id

---
+++ backend/src/main/java/com/coders/backers/plataformapython/backend/repository/ModuleRepository.java
@@ -0,0 +1,17 @@
+package com.coders.backers.plataformapython.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.coders.backers.plataformapython.backend.models.ModuleModel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModuleRepository  extends JpaRepository<ModuleModel, Long> {
+    List<ModuleModel> findByActive(boolean active);
+    List<ModuleModel> findByTitle(String title);
+    List<ModuleModel> findByTitleContainingIgnoreCase(String title);
+    List<ModuleModel> findByOrdenGreaterThan(int orden);
+
+    boolean existsByTitle(String title);
+}
