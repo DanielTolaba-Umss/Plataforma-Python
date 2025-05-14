cc3f2c38a5ecf3f93d5591c136d4f6dddb035e33
JhonatanEQ <encinas.jhonatan.dev@gmail.com>
2025-05-13 22:40:17 -0400

    FEAT (jhonatan): Refactorizar crud docente

--- backend/src/main/java/com/coders/backers/plataformapython/backend/models/userModel/TeacherEntity.java
+++ backend/src/main/java/com/coders/backers/plataformapython/backend/models/userModel/TeacherEntity.java
@@ -7,10 +7,6 @@ import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.coders.backers.plataformapython.backend.enums.Role;
-
-import com.coders.backers.plataformapython.backend.models.ModuleEntity;
-
 import jakarta.persistence.*;
 
 @Getter
@@ -23,14 +19,6 @@ public class TeacherEntity extends UserEntity {
 
     @Column(name = "specialty")
     private String specialty;
-    
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-        name = "teacher_module",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "module_id")
-    )
-    private Set<ModuleEntity> modules = new HashSet<>();
     
     
     public TeacherEntity(String specialty) {
