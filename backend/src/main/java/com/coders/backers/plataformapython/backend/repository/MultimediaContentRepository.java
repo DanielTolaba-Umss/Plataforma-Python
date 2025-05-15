cdd469719c1e38b5d5e61c54f4b501ae9b816f30
Mauricio Erick Molina Beltran <maumbeltran@gmail.com>
2025-05-15 12:07:08 -0400

    without multimedia content

--- backend/src/main/java/com/coders/backers/plataformapython/backend/repository/MultimediaContentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coders.backers.plataformapython.backend.repository;
-
-import com.coders.backers.plataformapython.backend.models.MultimediaContentModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MultimediaContentRepository extends JpaRepository<MultimediaContentModel, Long> {
-}
