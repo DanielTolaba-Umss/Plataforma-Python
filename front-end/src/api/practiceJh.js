e7d720bb5be33faf62012f08c653652b8c6ed535
Encinas-666 <202103264@est.umss.edu>
2025-06-11 00:27:39 -0400

    crear servicios

---
+++ front-end/src/api/practiceJh.js
@@ -0,0 +1,3 @@
+import { API_URL } from "./configuracion";
+
+const PRACTICE_API_URL = `${API_URL}/practice`;
---
+++ front-end/src/api/tryPracticeService.js
@@ -0,0 +1,51 @@
+import { API_URL } from "./configuracion";
+
+const TRY_PRACTICE_API_URL = `${API_URL}/try-practice`;
+
+export const tryPracticeService = {
+    async getAllPractices() {
+        try {
+            const response = await fetch(`${TRY_PRACTICE_API_URL}/all`);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error("Error fetching all practices:", error);
+            throw error;
+        }
+    },
+
+    async createTryPractice(payload) {
+        try {
+            const response = await fetch(TRY_PRACTICE_API_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            });
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error("Error creating practice:", error);
+            throw error;
+        }
+    },
+    
+
+    async getPracticeById(id) {
+        try {
+            const response = await fetch(`${TRY_PRACTICE_API_URL}/${id}`);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(`Error fetching practice with ID ${id}:`, error);
+            throw error;
+        }
+    }
+};
