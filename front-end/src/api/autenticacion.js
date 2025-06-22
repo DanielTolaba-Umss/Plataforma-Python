eeb7a7998e3c49b52876540783d3d44bc8d14963
Yesica <sanchezmaria2c@gmail.com>
2025-06-22 16:29:55 -0400

    eliminar archivo

--- front-end/src/api/autenticacion.js
+++
@@ -1,7 +0,0 @@
-import api from './configuracion';
-
-export const autenticacionAPI = {
-  login: (datos) => api.post('/auth/login', datos),
-  refreshToken: (refreshToken) => api.post('/auth/refresh-token', { refreshToken }),
-  cambiarContrasena: (email, datos) => api.put(`/auth/change-password/${email}`, datos),
-};
